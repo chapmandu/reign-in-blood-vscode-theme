aa907a93e26859ec18b7c8f449202c3391c697c1
Adam Chapman <adam.p.chapman@gmail.com>
2020-12-16 09:17:00 +1100

    adds vue sample

---
+++ samples/vue.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="slayer">Component {{title}}</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: "Reign in Blood"
+        };
+    }
+}
+</script>
+
+<style>
+.slayer {
+    background-color: #b30317;
+    font-size: 48px;
+}
+</style>
